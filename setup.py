839c1ec0ded453b414117bedef9a769903f9d1ce
Amitesh Gangrade <amiteshgangrade@Amiteshs-MBP.lan>
2024-09-30 22:59:47 -0500

    Changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="optimiz",
-    version="0.1.0",
+    version="0.1.1",
     author="Amitesh Gangrade",
     author_email="gangradeamitesh@gmail.com",
     description="A simple optimization library for machine learning",
