e1f5d58d89ad9e7e61fbd74051d678420259c718
Amitesh Gangrade <gangradeamitesh@gmail.com>
2024-11-13 14:12:16 -0600

    Shitty Parallelism

--- optimiz/facility_location.pyx
+++ optimiz/facility_location.pyx
@@ -1,6 +1,21 @@
 import numpy as np
 cimport numpy as cnp
 from sklearn.metrics import pairwise_distances
+from cython.parallel import prange
+
+
+def parallel_get_gain(int n ,double[: , :] simi_x ,double[:] current_values):
+    #cdef double[:] gains = np.zeros(n , dtype='float64')
+    cdef int i , j
+    cdef double max_value , gain_sum
+    cdef double[:] partial_gain = np.zeros(n , dtype='float64')
+    for i in prange(n , nogil=True):
+        partial_gain[i] = 0.0
+        for j in range(len(current_values)):
+            max_value = max(simi_x[i, j], current_values[j])
+            partial_gain[i] += max_value
+
+    return partial_gain
 
 
 class FacilityLocation:
@@ -15,12 +30,15 @@ class FacilityLocation:
         self.selected_indices = set()
 
     def calculate_gain(self) -> np.ndarray:
-        cdef cnp.ndarray gains = np.zeros(self.n , dtype = 'float64')
-        for i in range(self.n):
-            if i in self.selected_indices:
-                continue
-            gains[i] = np.maximum(self.X_simi[i] , self.current_values).sum()
-        return gains
+        # cdef cnp.ndarray gains = np.zeros(self.n , dtype = 'float64')
+        # n = self.n
+        # for i in range(n):
+        #     if i in self.selected_indices:
+        #         continue
+        #     gains[i] = np.maximum(self.X_simi[i] , self.current_values).sum()
+        # return gains
+        
+        return parallel_get_gain(self.n , self.X_simi , self.current_values)
 
     def compute_simi_matrix(self) -> np.ndarray:
         X_pairwise = pairwise_distances(self.X, metric="euclidean", squared=True)
@@ -28,8 +46,13 @@ class FacilityLocation:
     def select(self, int subset_size) -> np.ndarray: 
         cdef cnp.ndarray[cnp.float64_t , ndim=1] gains
         for _ in range(subset_size):
-            gains = self.calculate_gain()
+            #gains = np.asarray(self.calculate_gain())
+            gains = np.asarray(self.calculate_gain())
             best_idx = np.argmax(gains)
             self.selected_indices.add(best_idx)
             self.current_values = np.maximum(self.current_values , self.X_simi[best_idx])
         return self.selected_indices
+    # def convert_mem_to_numpy(self , double[:] mem_view) -> np.ndarray:
+
+    #     cdef cnp.ndarray[cnp.float64_t, ndim=1] numpy_array = np.asarray(mem_view)
+    #     return numpy_array
